5db69a18ba6845847cc3c93a87cd873add7b1f4e
Hiromi Takahashi <hiromix222@gmail.com>
2017-10-20 18:18:49 +0900

    #16 初回コミット。メッセージ一覧をテーブル表示するところまで

--- src/main/java/com/lsken/lskenapp/MessageListController.java
+++ src/main/java/com/lsken/lskenapp/MessageListController.java
@@ -1,53 +1,26 @@
 package com.lsken.lskenapp;
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lsken.lskenapp.domain.Message;
+import com.lsken.lskenapp.service.MessageService;
+
 import org.springframework.ui.Model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 @Controller
-//@EnableAutoConfiguration
-@RequestMapping("/messagelist")
 public class MessageListController {
-	  @RequestMapping(method = RequestMethod.GET)
+	@Autowired
+	MessageService messageService;
+
+	  @RequestMapping("/messagelist")
 	    public String index(Model model) {
-	        ArrayList <ViewData> list = new ArrayList<ViewData>();
-	        for (int i = 0; i < 5; i++) {
-	            ViewData data = new ViewData();
-	            StringBuffer buffer = new StringBuffer();
-	            buffer.append("メッセージ");
-	            buffer.append(i);
-	            data.setNo(i + 1);
-	            data.setMessage(buffer.toString());
-	            list.add(data);
-	        }
+		  List<Message> messages = messageService.findAllOrderByPostDate();
 	 
-	        model.addAttribute("msg", list);
+	        model.addAttribute("msgs", messages);
 	        return "messagelistview";
-	    }
-	}
-	 
-	class ViewData {
-	    private int no;
-	    private String message;
-	 
-	    public int getNo() {
-	        return no;
-	    }
-	 
-	    public void setNo(int no) {
-	        this.no = no;
-	    }
-	 
-	    public String getMessage() {
-	        return message;
-	    }
-	 
-	    public void setMessage(String message) {
-	        this.message = message;
-	    }
-	 
-	    public String toString() {
-	        return message;
 	    }
 	}
