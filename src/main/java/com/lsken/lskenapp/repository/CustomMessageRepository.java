c9061b630169ff0de361d5613f0ef988276c5a7a
Hiromi Takahashi <hiromix222@gmail.com>
2018-01-18 10:49:10 +0900

    #9斎藤さんソースマージ

--- src/main/java/com/lsken/lskenapp/repository/CustomMessageRepository.java
+++ src/main/java/com/lsken/lskenapp/repository/CustomMessageRepository.java
@@ -14,8 +14,9 @@ public interface CustomMessageRepository extends JpaRepository<CustomMessage, In
 	
 	@Query(value="SELECT  MESSAGE_ID, u.USER_NAME FROM_USER_NAME , GROUP_ID, CASE TYPE WHEN 'text' THEN MESSAGE_DETAIL WHEN 'map' THEN MESSAGE_DETAIL WHEN 'stamp' THEN DATA END AS MESSAGE_DETAIL,TYPE, POST_DATE FROM MESSAGES m LEFT OUTER JOIN STAMPS s ON  (s.FILENAME = m.MESSAGE_DETAIL) , USERS u where m.FROM_USER_ID = u.USER_ID AND m.MESSAGE_ID = :ID ORDER BY POST_DATE ASC", nativeQuery=true)  
 	public CustomMessage findMergeById(@Param("ID")int id);
+	
 	/**
-	 * 指定IDより最新のメッセージを取得(指定件数)
+	 * 指定IDより基準のメッセージを取得(指定件数)
 	 * @param offset
 	 * @param limit
 	 * @return
@@ -28,34 +29,13 @@ public interface CustomMessageRepository extends JpaRepository<CustomMessage, In
 			+ "FROM MESSAGES m "
 			+ "LEFT OUTER JOIN STAMPS s ON  (s.FILENAME = m.MESSAGE_DETAIL) , "
 			+ "USERS u "
-			+ "where m.FROM_USER_ID = u.USER_ID and m.MESSAGE_ID > (?1) "
+			+ "where m.FROM_USER_ID = u.USER_ID and ((?1 >= 0 and m.MESSAGE_ID > ABS(?1)) or (?1 < 0 and m.MESSAGE_ID < ABS(?1)))"
 			+ "ORDER BY POST_DATE ASC "
 			+ ") t "
 			+ "where rownum <= (?2) "
-			+ "order by t.post_Date", 
+			+ "order by "
+			+ "case ?3 when 0 then t.post_Date end,"
+			+ "case ?3 when 1 then t.post_Date end desc", 
 			nativeQuery=true)
-	List<CustomMessage> findLatestLimit(Integer offset, Integer limit);
-	
-	/**
-	 * 指定IDより前のメッセージを取得(指定件数)
-	 * @param offset
-	 * @param limit
-	 * @return
-	 */
-	@Query(value="select * from ( "
-			+ "SELECT "
-			+ "MESSAGE_ID, u.USER_NAME FROM_USER_NAME , GROUP_ID, "
-			+ "CASE TYPE WHEN 'text' THEN MESSAGE_DETAIL WHEN 'map' THEN MESSAGE_DETAIL WHEN 'stamp' THEN DATA END AS MESSAGE_DETAIL,"
-			+ "TYPE, POST_DATE "
-			+ "FROM MESSAGES m "
-			+ "LEFT OUTER JOIN STAMPS s ON  (s.FILENAME = m.MESSAGE_DETAIL) , "
-			+ "USERS u "
-			+ "where m.FROM_USER_ID = u.USER_ID and m.MESSAGE_ID < (?1) "
-			+ "ORDER BY POST_DATE ASC"
-			+ ") t "
-			+ "where rownum <= (?2) "
-			+ "order by t.post_Date", 
-			nativeQuery=true)
-	List<CustomMessage> findLatestLimitBefore(Integer offset, Integer limit);
-	
+	List<CustomMessage> findLatestLimit(Integer offset, Integer limit, Integer sort);
 }
--- src/main/java/com/lsken/lskenapp/service/MessageService.java
+++ src/main/java/com/lsken/lskenapp/service/MessageService.java
@@ -39,32 +39,19 @@ public class MessageService {
 	public CustomMessage findOneMerge(int mesageId) {
 		return customuMessageRepository.findMergeById(mesageId);
 	}
-	
 	/**
-	 * 指定messageIdより最新を全て取得(POST日時の降順)
+	 * 指定messageIdを基準に一覧を取得
 	 * @param messageid
 	 * @return
 	 */
 	public List<CustomMessage> findItems(Integer offset, Integer limit, String sort) {
 		List<CustomMessage> list = new ArrayList<>();
-		if(offset >= 0)
-		{
-			// 指定IDよりも大きい一覧(件数指定)
-			list = customuMessageRepository.findLatestLimit(Math.abs(offset), limit);
-		} else {
-			// 指定IDよりも小さい一覧(件数指定)
-			list = customuMessageRepository.findLatestLimitBefore(Math.abs(offset), limit);
-		}
 		
-		// 降順に並び替え
-		if(sort.equals("-"))
-		{
-			list.sort((a,b)-> b.getPostDate().compareTo(a.getPostDate()));
-		};
+		// 指定IDを基準に一覧を取得
+		list = customuMessageRepository.findLatestLimit(offset, limit, sort.equals("-") ? 1 : 0);
 		
 		return list;
 	}
-	
 	public Message create(Message mesage) {
 		return messageRepository.save(mesage);
 	}
